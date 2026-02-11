33597bbe117dd0a07df22989d0bd97e3a078c060
duyupeng <duyupeng36@foxmail.com>
2026-02-11 08:11:25 +0800

    Destription: space.c - count space characters in input
    Change-Id: Id90f6257bbb910e2d9be3f103f85bfc5c588b775

---
+++ ch01/space.c
@@ -0,0 +1,25 @@
+/**
+ * space.c - count space characters in input
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    int ch;
+    long number_space = 0;
+    long number_tab = 0;
+    long number_newline = 0;
+
+    while((ch = getchar()) != EOF)
+    {
+        if (ch == ' ')
+            ++number_space;
+        else if (ch == '\t')
+            ++number_tab;
+        else if (ch == '\n')
+            ++number_newline;
+    }
+    printf("spaces: %ld, tabs: %ld, newlines: %ld\n",
+           number_space, number_tab, number_newline);
+}
